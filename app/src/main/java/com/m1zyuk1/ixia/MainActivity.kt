a8bfe4909e9bcbbda0738b24618eba4f2df04e81
mizune <willful.slight@gmail.com>
2018-03-11 23:23:11 +0900

    Enable on kotlin data binding

--- app/src/main/java/com/m1zyuk1/ixia/MainActivity.kt
+++ app/src/main/java/com/m1zyuk1/ixia/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.m1zyuk1.ixia
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+import android.databinding.DataBindingUtil
+import android.view.View
+import android.widget.Toast
+import com.m1zyuk1.ixia.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupUi()
+    }
+
+    private fun setupUi() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.cameraButton.setOnClickListener{
+            Toast.makeText(this,"Test button",Toast.LENGTH_SHORT).show()
+        }
     }
 }
