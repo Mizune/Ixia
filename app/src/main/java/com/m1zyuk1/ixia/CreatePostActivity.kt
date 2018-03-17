330490be6ea8317b054b52e46a4263dd6547adbd
mizune <willful.slight@gmail.com>
2018-03-17 23:52:28 +0900

    Enable binding

--- app/src/main/java/com/m1zyuk1/ixia/CreatePostActivity.kt
+++ app/src/main/java/com/m1zyuk1/ixia/CreatePostActivity.kt
@@ -2,12 +2,17 @@ package com.m1zyuk1.ixia
 
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.m1zyuk1.ixia.databinding.ActivityCreatePostBinding
 
 class CreatePostActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreatePostBinding
+
     companion object MakeIntent {
         val IMAGE_URL = "image_url"
         fun makeIntent(context: Context,imageUrl: String): Intent {
@@ -19,8 +24,14 @@ class CreatePostActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_post)
-        var url = intent.getStringExtra(IMAGE_URL) ?: "Unknown Uri"
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_create_post)
+        setupUi()
+    }
+
+    private fun setupUi(){
+        var url = intent.getStringExtra(IMAGE_URL) ?: ""
+        binding.imageView.setImageURI(Uri.parse(url))
         Toast.makeText(this,"Camera Uri = " + url ,Toast.LENGTH_SHORT).show()
+        supportActionBar?.title = "投稿する"
     }
 }
